dbede0e407023b8058eb32486fa9a46b4abb00a9
Tomas Doran <bobtfish@bobtfish.net>
2014-07-24 22:20:49 +0000

    Fix clean_all_containers

--- clean_all_containers.sh
+++ clean_all_containers.sh
@@ -3,5 +3,5 @@
 # Brutally murder all the running containers on a machine
 
 docker ps | awk '{print $1}' | xargs docker kill
-docker pa -s | awk '{print $1}' | xargs docker rm
+docker ps -a | awk '{print $1}' | xargs docker rm
 
