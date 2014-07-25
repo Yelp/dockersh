3b213f47c3e381f34b7199e403ee27fd46688592
Tomas Doran <bobtfish@bobtfish.net>
2014-07-25 03:15:23 +0000

    Try to start porting the nsenter code

--- nsenter_amd64.go
+++ nsenter_amd64.go
@@ -1,11 +1,13 @@
 package main
 
 import (
-	"fmt"
+	"github.com/coreos/go-namespaces/namespace"
+)
+import (
 	"os"
 	"os/exec"
-	"strconv"
 	"strings"
+	"syscall"
 )
 
 func nsenterdetect() (found bool, err error) {
@@ -23,13 +25,74 @@ func nsenterdetect() (found bool, err error) {
 
 func nsenterexec(pid int, uid int, gid int, wd string, shell string) (err error) {
 	// sudo nsenter --target "$PID" --mount --uts --ipc --net --pid --setuid $DESIRED_UID --setgid $DESIRED_GID --wd=$HOMEDIR -- "$REAL_SHELL"
-	cmd := exec.Command("sudo", "/usr/local/bin/nsenter",
-		"--target", strconv.Itoa(pid), "--mount", "--uts", "--ipc", "--net", "--pid",
-		"--setuid", strconv.Itoa(uid), "--setgid", strconv.Itoa(gid), fmt.Sprintf("--wd=%s", wd),
-		"--", shell)
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	err = cmd.Run()
-	return err
+	//cmd := exec.Command("sudo", "/usr/local/bin/nsenter",
+	//	"--target", strconv.Itoa(pid), "--mount", "--uts", "--ipc", "--net", "--pid",
+	//	"--setuid", strconv.Itoa(uid), "--setgid", strconv.Itoa(gid), fmt.Sprintf("--wd=%s", wd),
+	//	"--", shell)
+	//cmd.Stdin = os.Stdin
+	//cmd.Stdout = os.Stdout
+	//cmd.Stderr = os.Stderr
+	//err = cmd.Run()
+	//return err
+
+	/* FIXME: Make these an array and loop through them, as this is gross
+
+		/* --mount */
+	fd, err := namespace.OpenProcess(pid, namespace.CLONE_NEWNS)
+	if err != nil {
+		panic("namespace.OpenProcess(pid, namespace.CLONE_NEWNS)")
+	}
+	namespace.Setns(fd, namespace.CLONE_NEWNS)
+	namespace.Close(fd)
+
+	/* --uts */
+	fd, err = namespace.OpenProcess(pid, namespace.CLONE_NEWUTS)
+	/* TODO: Check errors */
+	namespace.Setns(fd, namespace.CLONE_NEWUTS)
+	namespace.Close(fd)
+
+	/* --ipc */
+	fd, err = namespace.OpenProcess(pid, namespace.CLONE_NEWIPC)
+	/* TODO: Check errors */
+	namespace.Setns(fd, namespace.CLONE_NEWIPC)
+	namespace.Close(fd)
+
+	/* --net */
+	fd, err = namespace.OpenProcess(pid, namespace.CLONE_NEWNET)
+	/* TODO: Check errors */
+	namespace.Setns(fd, namespace.CLONE_NEWNET)
+	namespace.Close(fd)
+
+	/* --pid */
+	fd, err = namespace.OpenProcess(pid, namespace.CLONE_NEWPID)
+	/* TODO: Check errors */
+	namespace.Setns(fd, namespace.CLONE_NEWPID)
+	namespace.Close(fd)
+
+	/* END FIXME */
+
+	if gid > 0 {
+		err = syscall.Setgroups([]int{}) /* drop supplementary groups */
+		if err != nil {
+			panic("setgroups failed")
+		}
+		err = syscall.Setgid(gid)
+		if err != nil {
+			panic("setgid failed")
+		}
+	}
+	if uid > 0 {
+		err = syscall.Setuid(uid)
+		if err != nil {
+			panic("setuid failed")
+		}
+	}
+
+	args := []string{shell}
+	env := os.Environ()
+	execErr := syscall.Exec(shell, args, env)
+	if execErr != nil {
+		panic(execErr)
+	}
+	return execErr
 }
