0ccc551ef47868d5a96f577198581ade2b34d7c2
Tomas Doran <bobtfish@bobtfish.net>
2014-08-30 10:30:16 +0000

    Add the start of more sane tests

---
+++ docker_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"testing"
+	"fmt"
+)
+
+func Test_dockerPid_1(t *testing.T) {
+	pid, err := dockerpid("testcontainer")
+	if err != nil {
+		t.Error(fmt.Sprintf("Error from dockerpid: %v", err))
+	}
+	if pid != 666 {
+		t.Error(fmt.Sprintf("PID was %i expected 666", pid))
+	}
+}
+
+func Test_dockerSha_1(t *testing.T) {
+	sha, err := dockersha("testcontainer")
+	if err != nil {
+		t.Error(fmt.Sprintf("Error from dockersha: %v", err))
+	}
+	if sha != "666" {
+		t.Error(fmt.Sprintf("SHA was %s expected 666", sha))
+	}
+}
+
+func Test_dockerStart(t *testing.T) {
+	pid, err := dockerstart("someuser", "homedirfrom", "homedirto", "name", "container", "dockersock", true, true, true, "init", []string{}, []string{})
+	if err != nil {
+                t.Error(fmt.Sprintf("Error from dockerstart: %v", err))
+	}
+	if pid != 666 {
+                t.Error(fmt.Sprintf("PID was %i expected 666", pid))
+        }
+}
+
+
