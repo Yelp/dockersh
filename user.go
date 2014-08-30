b318bfc91d300753e86558cf9b4935cf217f1b23
Tomas Doran <bobtfish@bobtfish.net>
2014-08-30 10:18:50 +0000

    Clean up printing to stdout

--- user.go
+++ user.go
@@ -3,7 +3,6 @@ package main
 import (
 	"errors"
 	"fmt"
-	"os"
 	"os/user"
 	"strconv"
 )
@@ -11,19 +10,16 @@ import (
 func getCurrentUser() (username string, homedir string, uid int, gid int, err error) {
 	user, err := user.Current()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "could not get current user: %v", err)
-		return "", "", 0, 0, errors.New("could not get current user")
+		return "", "", 0, 0, errors.New(fmt.Sprintf("could not get current user: %v", err))
 	}
 	return getUser(user)
 }
 
 func getUser(user *user.User) (username string, homedir string, uid int, gid int, err error) {
 	if user.HomeDir == "" {
-		fmt.Fprintf(os.Stderr, "didn't get a home directory")
 		return "", "", 0, 0, errors.New("didn't get a home directory")
 	}
 	if user.Username == "" {
-		fmt.Fprintf(os.Stderr, "didn't get a username")
 		return "", "", 0, 0, errors.New("didn't get a username")
 	}
 	uid, err = strconv.Atoi(user.Uid)
