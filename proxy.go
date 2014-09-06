8ab43564ff853754181d004eaec6fb9d9a8044b0
Tomas Doran <tdoran@yelp.com>
2014-09-06 14:17:35 +0100

    go fmt

--- proxy.go
+++ proxy.go
@@ -1,96 +1,94 @@
 package main
 
 import (
-    "bytes"
-    "encoding/hex"
-    "fmt"
-    "io"
-    "log"
-    "net"
+	"bytes"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"log"
+	"net"
 )
 
 func proxyConn(remoteAddr string, conn *net.TCPConn) {
-    rAddr, err := net.ResolveTCPAddr("tcp", remoteAddr)
-    if err != nil {
-        panic(err)
-    }
+	rAddr, err := net.ResolveTCPAddr("tcp", remoteAddr)
+	if err != nil {
+		panic(err)
+	}
 
-    rConn, err := net.DialTCP("tcp", nil, rAddr)
-    if err != nil {
-        panic(err)
-    }
-    defer rConn.Close()
+	rConn, err := net.DialTCP("tcp", nil, rAddr)
+	if err != nil {
+		panic(err)
+	}
+	defer rConn.Close()
 
-    buf := &bytes.Buffer{}
-    for {
-        data := make([]byte, 256)
-        n, err := conn.Read(data)
-        if err != nil {
-            panic(err)
-        }
-        buf.Write(data[:n])
-        if data[0] == 13 && data[1] == 10 {
-            break
-        }
-    }
+	buf := &bytes.Buffer{}
+	for {
+		data := make([]byte, 256)
+		n, err := conn.Read(data)
+		if err != nil {
+			panic(err)
+		}
+		buf.Write(data[:n])
+		if data[0] == 13 && data[1] == 10 {
+			break
+		}
+	}
 
-    if _, err := rConn.Write(buf.Bytes()); err != nil {
-        panic(err)
-    }
-    log.Printf("sent:\n%v", hex.Dump(buf.Bytes()))
+	if _, err := rConn.Write(buf.Bytes()); err != nil {
+		panic(err)
+	}
+	log.Printf("sent:\n%v", hex.Dump(buf.Bytes()))
 
-    data := make([]byte, 1024)
-    n, err := rConn.Read(data)
-    if err != nil {
-        if err != io.EOF {
-            panic(err)
-        } else {
-            log.Printf("received err: %v", err)
-        }
-    }
-    log.Printf("received:\n%v", hex.Dump(data[:n]))
+	data := make([]byte, 1024)
+	n, err := rConn.Read(data)
+	if err != nil {
+		if err != io.EOF {
+			panic(err)
+		} else {
+			log.Printf("received err: %v", err)
+		}
+	}
+	log.Printf("received:\n%v", hex.Dump(data[:n]))
 }
 
 func handleConn(remoteAddr string, in <-chan *net.TCPConn, out chan<- *net.TCPConn) {
-    for conn := range in {
-        proxyConn(remoteAddr, conn)
-        out <- conn
-    }
+	for conn := range in {
+		proxyConn(remoteAddr, conn)
+		out <- conn
+	}
 }
 
 func closeConn(in <-chan *net.TCPConn) {
-    for conn := range in {
-        conn.Close()
-    }
+	for conn := range in {
+		conn.Close()
+	}
 }
 
 func proxyMain(localAddr string, remoteAddr string) {
-    fmt.Printf("Listening: %v\nProxying: %v\n\n", localAddr, remoteAddr)
+	fmt.Printf("Listening: %v\nProxying: %v\n\n", localAddr, remoteAddr)
 
-    addr, err := net.ResolveTCPAddr("tcp", localAddr)
-    if err != nil {
-        panic(err)
-    }
+	addr, err := net.ResolveTCPAddr("tcp", localAddr)
+	if err != nil {
+		panic(err)
+	}
 
-    listener, err := net.ListenTCP("tcp", addr)
-    if err != nil {
-        panic(err)
-    }
+	listener, err := net.ListenTCP("tcp", addr)
+	if err != nil {
+		panic(err)
+	}
 
-    pending, complete := make(chan *net.TCPConn), make(chan *net.TCPConn)
+	pending, complete := make(chan *net.TCPConn), make(chan *net.TCPConn)
 
-    for i := 0; i < 5; i++ {
-        go handleConn(remoteAddr, pending, complete)
-    }
-    go closeConn(complete)
+	for i := 0; i < 5; i++ {
+		go handleConn(remoteAddr, pending, complete)
+	}
+	go closeConn(complete)
 
-    for {
-        conn, err := listener.AcceptTCP()
-        if err != nil {
-            panic(err)
-        }
-        pending <- conn
-    }
+	for {
+		conn, err := listener.AcceptTCP()
+		if err != nil {
+			panic(err)
+		}
+		pending <- conn
+	}
 }
-
-
