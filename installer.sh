86ef32650fc50771146441d6e0a41cb1dc3c0536
Tomas Doran <bobtfish@bobtfish.net>
2014-10-14 11:45:32 +0000

    Fix the installer

--- installer.sh
+++ installer.sh
@@ -5,14 +5,14 @@ echo ""
 echo "To install dockersh"
 echo "   docker run -v /usr/local/bin:/target thiscontainer"
 echo "If you're using the publicly available (built from source) container, this is:"
-echo "   docker run -v /usr/local/bin:/target Yelp/dockersh"
+echo "   docker run -v /usr/local/bin:/target yelp/dockersh"
 echo ""
 
 if [ -d "/target" ];then
   echo "GOING TO DO INSTALL IN 5 SECONDS, Ctrl-C to abort"
   sleep 5
   rm -f /target/dockersh
-  cp -a /gopath/src/dockersh/dockersh /target/dockersh
+  cp -a /dockersh /target/dockersh
 else
   echo "No /target directory found, not installing"
 fi
